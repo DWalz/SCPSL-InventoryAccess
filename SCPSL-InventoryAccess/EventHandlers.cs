934d659f705895d80d9d46c9c13f7ac41ca564f4
DWalz <d.walz@zoho.com>
2020-04-16 00:43:28 +0200

    Add Plugin

---
+++ SCPSL-InventoryAccess/EventHandlers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using EXILED;
+using EXILED.Extensions;
+
+namespace SCPSL_InventoryAccess
+{
+    public class EventHandlers
+    {
+        public void OnPlayerDoorInteract(ref DoorInteractionEvent ev)
+        {
+            ev.Allow = hasPermission(ev.Player, ev.Door.permissionLevel);
+        }
+
+        public void OnPlayerLockerInteract(LockerInteractionEvent ev)
+        {
+            ev.Allow = hasPermission(ev.Player, ev.Locker.chambers[0].accessToken);
+        }
+
+        private bool hasPermission(ReferenceHub player, String requested)
+        {
+            if (requested == "")
+            {
+                return true;
+            }
+            foreach (Inventory.SyncItemInfo itemInfo in player.GetAllItems())
+            {
+                if (player.inventory.GetItemByID(itemInfo.id).permissions.Contains(requested))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ SCPSL-InventoryAccess/Plugin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using EXILED;
+
+namespace SCPSL_InventoryAccess
+{
+    public class InventoryAccessPlugin : Plugin
+    {
+
+        private string _name = "InventoryAccess";
+        private string _prefix = "[InvAcc] ";
+
+        private EventHandlers _handlers;
+        
+        public override void OnEnable()
+        {
+            Log.Info(_prefix + "Enabling InventoryAccess");
+            Log.Info(_prefix + "Registering event handlers");
+            try
+            {
+                _handlers = new EventHandlers();
+                Events.DoorInteractEvent += _handlers.OnPlayerDoorInteract;
+                Events.LockerInteractEvent += _handlers.OnPlayerLockerInteract;
+            }
+            catch (Exception e)
+            {
+                Log.Error(_prefix + "Registering event handlers failed!");
+            }
+        }
+
+        public override void OnDisable()
+        {
+            Log.Info(_prefix + "De-registering event handlers");
+            Events.DoorInteractEvent -= _handlers.OnPlayerDoorInteract;
+            Events.LockerInteractEvent -= _handlers.OnPlayerLockerInteract;
+            _handlers = null;
+        }
+
+        public override void OnReload()
+        {
+            OnDisable();
+            OnEnable();
+        }
+
+        public override string getName { get => _name; }
+    }
+    
+    
+}
